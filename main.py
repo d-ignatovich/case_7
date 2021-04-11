ceaa2beab91abbf247be2102ce920253e25ce7a3
d-ignatovich <73305627+d-ignatovich@users.noreply.github.com>
2021-04-12 00:15:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 """Case-study 
-Developers:   Ignatovich D. (%),
+Developers:   Ignatovich D. (60%),
               Miller A. (30%),
               Poylova E. (40%)
 """ 
-s = 'Это утро, радость эта, Эта мощь и дня и света, Этот синий свод, Этот крик и вереницы, Эти стаи, эти птицы, Этот говор вод, Эти ивы и березы, Эти капли - эти слезы, Этот пух - не лист, Эти горы, эти долы, Эти мошки, эти пчелы, Этот зык и свист, Эти зори без затменья, Этот вздох ночной селенья, Эта ночь без сна, Эта мгла и жар постели, Эта дробь и эти трели,Это всё - весна.'
 import re
-s = re.sub(r'\s+(?=(?:[,.?!:;… -;:]))', r'', s)
+import random
+
+def open_initial_text():
+    with open('input.txt', "r") as f_in:
+        initial_text = ''
+        for lines in f_in:
+            lines.replace("\n", ' ')
+            initial_text += lines
+    return initial_text
+  
+text = open_initial_text()
+text = re.sub(r'\s+(?=(?:[,.?!:;… -;:]))', r'', text)
 marks = '''()-[]{};@#$%:'"\/^&amp;*_'''
-for i in s:
+for i in text:
     if i in marks:
-         t = s.replace(i, "")
-         continue
+        text = text.replace(i, "")
+        continue
     else:
         continue
 
-def open_initial_text(filename):
-    with open('input.txt', "r") as f_in:
-        initial_text = f_in.read()
-    return initial_text
-
-def clean_initial(initial):
-    initial = initial.replace("\n", " ")
-        
 text = t.split(' ')
 
 #Create a list of unique words.
@@ -46,21 +48,22 @@ for i in range(len(words)):
     word_connections.append(next_word)
 
 #Generating a delusional text.
-number_sentence = int(input('Введите количество предложений для генирации текста: '))
-for i in range(0, number_sentence):
-    x = random.randint(0, len(start_list)-1)
-    sentence = start_list[x]
-    x = words.index(sentence)
-    for e in range (19):
-        if len(word_connections[x])>0:
-            b = random.randint(0, len(word_connections[x])-1)
-        else:
-            b = 0
-        sentence += ' ' + str(word_connections[x][b])
-        if '.' in word_connections[b]:
-            break
-        if e == 18:
-            sentence += '.'
-        x = words.index(word_connections[x][b])
-    print(sentence)
+with open('output.txt', 'w') as f_out:
+    number_sentence = int(input('Введите количество предложений для генирации текста: '))
+    for i in range(0, number_sentence):
+        x = random.randint(0, len(start_list)-1)
+        sentence = start_list[x]
+        x = words.index(sentence)
+        for e in range (19):
+            if len(word_connections[x]) > 2:
+                b = random.randint(0, len(word_connections[x])-1)
+            else:
+                b = 0
+            sentence += ' ' + str(word_connections[x][b])
+            if '.' in word_connections[b]:
+                break
+            elif e == 18:
+                sentence += '.'
+            x = words.index(word_connections[x][b])
+        print(sentence, end = ' ', file = f_out)
 
